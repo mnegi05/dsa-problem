05b8b565861e98ccae1d36a788ec12bec5099fcc
mnegi05 <manishnegi572@gmail.com>
2026-04-25 13:38:50 +0530

    Palindrome Number Problem

---
+++ src/main/java/com/mnegi05/dsa_problem/palindrome_number/Solution.java
@@ -0,0 +1,22 @@
+package com.mnegi05.dsa_problem.palindrome_number;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        // Negative or ends with 0 (except 0)
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == reversedHalf
+        // For odd digits: x == reversedHalf / 10
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+}
