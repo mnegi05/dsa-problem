3248daeedab0e11bd4774948c4db4565471a641c
mnegi05 <manishnegi572@gmail.com>
2026-04-25 13:49:10 +0530

    Three Sum Problem

---
+++ src/main/java/com/mnegi05/dsa_problem/three_sum/Solution.java
@@ -0,0 +1,51 @@
+package com.mnegi05.dsa_problem.three_sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+        // System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate first element
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result.add(Arrays.asList(
+                            nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
