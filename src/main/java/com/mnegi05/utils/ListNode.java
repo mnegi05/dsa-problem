d0c0466f0b1a1b80dce4521c97fdabffad3bdbd3
mnegi05 <manishnegi572@gmail.com>
2026-04-25 13:34:40 +0530

    Add Two Number Problem Utils

---
+++ src/main/java/com/mnegi05/utils/ListNode.java
@@ -0,0 +1,20 @@
+package com.mnegi05.utils;
+
+// Definition for singly-linked list.
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
