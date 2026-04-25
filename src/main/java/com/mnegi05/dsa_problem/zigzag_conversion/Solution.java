477f932635557a9ad90e7730cb1cc95bad0bd858
mnegi05 <manishnegi572@gmail.com>
2026-04-25 13:37:53 +0530

    ZigZag Conversion Problem

---
+++ src/main/java/com/mnegi05/dsa_problem/zigzag_conversion/Solution.java
@@ -0,0 +1,25 @@
+package com.mnegi05.dsa_problem.zigzag_conversion;
+
+class Solution {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1)
+            return s;
+
+        StringBuilder result = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int j = row; j < n; j += cycleLen) {
+                result.append(s.charAt(j));
+
+                int second = j + cycleLen - 2 * row;
+                if (row != 0 && row != numRows - 1 && second < n) {
+                    result.append(s.charAt(second));
+                }
+            }
+        }
+        return result.toString();
+    }
+}
