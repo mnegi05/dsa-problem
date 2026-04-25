b02a6446260d1437c5b3e795baf0f6ea0fd54b4b
mnegi05 <manishnegi572@gmail.com>
2026-04-25 13:38:14 +0530

    Reverse Integer Problem

---
+++ src/main/java/com/mnegi05/dsa_problem/reverse_integer/Solution.java
@@ -0,0 +1,30 @@
+package com.mnegi05.dsa_problem.reverse_integer;
+
+class Solution {
+    public int reverse(int x) {
+
+        int reversed = 0;
+
+        while (x != 0) {
+
+            int digit = x % 10;
+            x /= 10;
+
+            // Check positive overflow
+            if (reversed > Integer.MAX_VALUE / 10 ||
+                    (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            // Check negative overflow
+            if (reversed < Integer.MIN_VALUE / 10 ||
+                    (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    }
+}
