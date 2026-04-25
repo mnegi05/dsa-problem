39c150a1026435c50ab5034b646b6fb9df5ec4fa
mnegi05 <manishnegi572@gmail.com>
2026-04-25 12:45:56 +0530

    dsa problem initial commit

---
+++ src/main/java/com/mnegi05/dsa_problem/DsaProblemApplication.java
@@ -0,0 +1,13 @@
+package com.mnegi05.dsa_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaProblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnegi05/dsa_problem/DsaProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnegi05.dsa_problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
