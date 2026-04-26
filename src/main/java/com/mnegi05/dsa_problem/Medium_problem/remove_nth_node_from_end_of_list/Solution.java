220e6f63ee99c3ec2906fd3aadb6b48f72b10215
mnegi05 <manishnegi572@gmail.com>
2026-04-26 20:53:31 +0530

    Remove Nth Node From End of List Problem

---
+++ src/main/java/com/mnegi05/dsa_problem/Medium_problem/remove_nth_node_from_end_of_list/Solution.java
@@ -0,0 +1,39 @@
+package com.mnegi05.dsa_problem.Medium_problem.remove_nth_node_from_end_of_list;
+
+import com.mnegi05.utils.ListNode;
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        // Dummy node to handle edge cases
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both pointers
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // Delete nth node from end
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+}
